2ee1e1ba5bf0739e56427df079b3074dcf65b56a
Pramod Kaushik Mudrakarta <27796676+pramodkaushik@users.noreply.github.com>
2018-04-23 01:32:23 -0500

    Update model.py

--- neural_programmer/model.py
+++ neural_programmer/model.py
@@ -1,4 +1,5 @@
 # Copyright 2016 Google Inc. All Rights Reserved.
+# Modifications copyright 2018 Pramod Kaushik Mudrakarta. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Author: aneelakantan (Arvind Neelakantan)
+
+"""Authors: aneelakantan (Arvind Neelakantan), pramodkaushik (Pramod Kaushik Mudrakarta)
 """
 
 from __future__ import print_function
